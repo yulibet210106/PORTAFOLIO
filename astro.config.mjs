a872145b5b17feee6d934b921ca26766e5284a78
Carolina Mendoza <yulibetquiroga@gmail.com>
2026-01-30 07:05:22 -0300

    Construyendo mi Portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yulibet210106.github.io",
+  base:"/PORTAFOLIO/",
   vite: {
     plugins: [tailwindcss()],
   },
